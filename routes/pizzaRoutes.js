3da360e026803d0561b7668ceac63b667bce3bac
Paretooptimal22 <you.k2208@gmail.com>
2019-11-24 17:37:24 -0800

    added restful routing to pizzaRoutes.js

--- routes/pizzaRoutes.js
+++ routes/pizzaRoutes.js
@@ -1,9 +1,21 @@
 module.exports = app {
   // GET all burgers
+  app.get(`./burgers`, (req, res) => {
+
+  })
 
   // POST one burger
+  app.post(`./burgers`, (req, res) => {
+
+  })
 
   // PUT one burger
+  app.put(`./burgers/:id`, (req, res) => {
+
+  })
 
   // DELETE one burger
+  app.delete(`./burgers/:id`, (req, res) => {
+
+  })
 }
