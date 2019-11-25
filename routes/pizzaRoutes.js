2ad4eb019422883713a6ab7be86624392b6340bc
Paretooptimal22 <you.k2208@gmail.com>
2019-11-24 17:46:52 -0800

    added res.send for testing

--- routes/pizzaRoutes.js
+++ routes/pizzaRoutes.js
@@ -1,21 +1,21 @@
 module.exports = app => {
   // GET all burgers
   app.get(`/burgers`, (req, res) => {
-
+    res.send(`GET all burgers`)
   })
 
   // POST one burger
   app.post(`/burgers`, (req, res) => {
-
+    res.send(`POST one burger`)
   })
 
   // PUT one burger
   app.put(`/burgers/:id`, (req, res) => {
-
+    res.send(`PUT one burger`)
   })
 
   // DELETE one burger
   app.delete(`/burgers/:id`, (req, res) => {
-
+    res.send(`DELETE one burger`)
   })
 }
