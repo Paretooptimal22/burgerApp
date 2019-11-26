32105c07de94ebda49abafaa35a9f62e51dea82b
Paretooptimal22 <you.k2208@gmail.com>
2019-11-25 19:46:43 -0800

    added get post put in app.js and changes to hbs

---
+++ public/assets/js/app.js
@@ -0,0 +1,46 @@
+const getBurgers = () => {
+  axios.get(`/burgers`)
+    .then(burgers => {
+      console.log(burgers)
+    })
+    .catch(e => console.error(e))
+}
+
+const addBurger = (name) => {
+  axios.post(`/burgers`, {
+    name,
+    eaten: 0
+  })
+    .then(() => {
+      console.log(`Burger Added!`)
+    })
+    .catch(e => console.error(e))
+}
+
+const eatBurger = id => {
+  axios.put(`/burgers/${id}`)
+    .then(() => {
+      console.log(`Burger Eaten!`)
+    })
+    .catch(e => console.error(e))
+}
+
+const removeBurger = () => {
+
+}
+
+document.getElementById(`addBurger`).addEventListener(`click`, e => {
+  e.preventDefault()
+  addBurger(document.getElementById(`burger`).value)
+  document.getElementById(`burger`).value = ``
+  window.location.reload()
+})
+
+document.addEventListener(`click`, e => {
+  if (e.target.parentNode.className.includes(`eatBurger`)) {
+    eatBurger(e.target.parentNode.dataset.burger)
+    window.location.reload()
+  } else if (e.target.parentNode.className.includes(`removeBurger`)) {
+    removeBurger()
+  }
+})
